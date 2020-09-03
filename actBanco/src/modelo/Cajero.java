3fe716f36967702a9537b7c5df9ea80357e908c7
Andrea Perez <32962201+Andrealpmz@users.noreply.github.com>
2020-09-03 14:56:20 -0500

    Create Cajero.java
    add atributo prueba

---
+++ actBanco/src/modelo/Cajero.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Universidad Felipe
+ */
+public class Cajero {
+    
+    private String Prueba;
+
+    /**
+     * Get the value of Prueba
+     *
+     * @return the value of Prueba
+     */
+    public String getPrueba() {
+        return Prueba;
+    }
+
+    /**
+     * Set the value of Prueba
+     *
+     * @param Prueba new value of Prueba
+     */
+    public void setPrueba(String Prueba) {
+        this.Prueba = Prueba;
+    }
+
+}
